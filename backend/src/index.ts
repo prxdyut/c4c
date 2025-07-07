1cbc41fde9bfae1415395e29b3b0d7961275531f
Pradyut <daspradyut516@gmail.com>
2025-07-07 04:31:28 +0000

    .

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,7 +7,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3005;
 
 // Middleware
 app.use(helmet());
